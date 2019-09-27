066c57652e8a43118b22f19a65e0f83f3dcb1b40
Lucas Lacerda <lucasalvlacerda@gmail.com>
2019-09-26 21:13:18 -0300

    remove auth

--- src/app.js
+++ src/app.js
@@ -6,6 +6,59 @@ const morgan = require('morgan');
 // const MongoClient = require('mongodb').MongoClient;
 
 
+const extendTimeoutMiddleware = (req, res, next) => {
+  const space = ' ';
+  let isFinished = false;
+  let isDataSent = false;
+
+  // Only extend the timeout for API requests
+  if (!req.url.includes('/api')) {
+    next();
+    return;
+  }
+
+  res.once('finish', () => {
+    isFinished = true;
+  });
+
+  res.once('end', () => {
+    isFinished = true;
+  });
+
+  res.once('close', () => {
+    isFinished = true;
+  });
+
+  res.on('data', (data) => {
+    // Look for something other than our blank space to indicate that real
+    // data is now being sent back to the client.
+    if (data !== space) {
+      isDataSent = true;
+    }
+  });
+
+  const waitAndSend = () => {
+    setTimeout(() => {
+      // If the response hasn't finished and hasn't sent any data back....
+      if (!isFinished && !isDataSent) {
+        // Need to write the status code/headers if they haven't been sent yet.
+        if (!res.headersSent) {
+          res.writeHead(202);
+        }
+
+        res.write(space);
+
+        // Wait another 15 seconds
+        waitAndSend();
+      }
+    }, 15000);
+  };
+
+  waitAndSend();
+  next();
+};
+
+
 app.use(morgan('combined'))
 
 const spellsUri = 'mongodb+srv://lucasalacerda:7894512@spells-mxwh5.gcp.mongodb.net/spellsdb?retryWrites=true&w=majority';
@@ -30,6 +83,7 @@ app.use('/', index);
 app.use('/api', authRoute);
 app.use('/api', userRoute);
 app.use('/api', spellsDeck);
+app.use(extendTimeoutMiddleware);
 
 
 app.listen(port, () => {
