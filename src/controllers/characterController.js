a9b3c9e9c9438e7ceb4103aef567a0054c7c222d
Lucas Lacerda <lucasalvlacerda@gmail.com>
2019-12-05 05:24:13 -0300

    fix spell

--- src/controllers/characterController.js
+++ src/controllers/characterController.js
@@ -8,7 +8,6 @@ exports.getAll = async (req, res, next) => {
         characters = await Character
             .find({})
             .populate('class', 'className -_id')
-            .populate('spells', 'title -_id, description -_id');
     } catch(err) {
         res.status(422).send(err);
     }
